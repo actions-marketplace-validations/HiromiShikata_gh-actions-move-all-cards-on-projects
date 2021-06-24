e2b6e360827358222168545fc0352772398e0bdb
HiromiShikata <shikata.hiromi@gmail.com>
2021-06-24 01:15:07 +0000

    feat: setup adapter

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,28 +1,22 @@
-import {wait} from '../src/wait'
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
-
-// shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
   process.env['INPUT_MILLISECONDS'] = '500'
+  process.env['INPUT_PROJECT_NAME'] = 'test-project'
+  process.env['INPUT_FROM_COLUMN_NAME'] = 'In progress'
+  process.env['INPUT_TO_COLUMN_NAME'] = 'To do'
+  process.env['INPUT_GITHUB_TOKEN'] = process.env.GH_TOKEN
+  process.env['GITHUB_REPOSITORY'] =
+    'HiromiShikata/gh-actions-move-all-cards-on-projects'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
     env: process.env
   }
+  console.log(cp.execFileSync(np, [ip], options).toString())
+  process.env['INPUT_FROM_COLUMN_NAME'] = 'To do'
+  process.env['INPUT_TO_COLUMN_NAME'] = 'In progress'
   console.log(cp.execFileSync(np, [ip], options).toString())
 })
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+import {MoveAllCardsOnProjectUseCase} from './MoveAllCardsOnProjectUseCase'
+import {OctokitGithubRepository} from './OctokitGithubRepository'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const projectName: string = core.getInput('project_name')
+    const fromColumnName: string = core.getInput('from_column_name')
+    const toColumnName: string = core.getInput('to_column_name')
+    const githubToken: string = core.getInput('github_token')
+    core.debug(
+      `will move issues to ${toColumnName} from ${fromColumnName} on ${projectName}`
+    )
+    const usecase = new MoveAllCardsOnProjectUseCase(
+      new OctokitGithubRepository(
+        github.context.repo.owner,
+        github.context.repo.repo,
+        githubToken
+      )
+    )
+    await usecase.execute(projectName, fromColumnName, toColumnName)
   } catch (error) {
     core.setFailed(error.message)
   }
